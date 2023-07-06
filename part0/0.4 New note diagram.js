eee3beb9b9cbbf820c4fb1374b05655e22e4d77a
SenadTopcic <61664503+SenadTopcic@users.noreply.github.com>
2023-07-06 16:50:07 +0100

    Add files via upload
    Mermaid

---
+++ part0/0.4 New note diagram.js
@@ -0,0 +1,34 @@
+sequenceDiagram
+    participant browser
+    participant server
+
+    browser->>server: GET https://studies.cs.helsinki.fi/exampleapp/notes
+    activate server
+    server-->>browser: HTML document
+    deactivate server
+
+    browser->>server: GET https://studies.cs.helsinki.fi/exampleapp/main.css
+    activate server
+    server-->>browser: the CSS file
+    deactivate server
+
+    browser->>server: GET https://studies.cs.helsinki.fi/exampleapp/spa.js
+    activate server
+    server-->>browser: the JavaScript file
+    deactivate server
+
+    Note right of browser: The browser starts executing the JavaScript code that fetches the JSON from the server
+
+    browser->>server: GET https://studies.cs.helsinki.fi/exampleapp/data.json
+    activate server
+    server-->>browser: [{..., {"content": "I am not stupid :)", "date": "2023-07-06T10:00:39.614Z"}, ... ]
+    deactivate server
+
+    Note right of browser: The browser executes the callback function that renders the notes
+
+    browser->>server: POST https://studies.cs.helsinki.fi/exampleapp/new_note
+    activate server
+    server-->>browser: Success response
+    deactivate server
+
+    Note over browser: User submits the form with data
