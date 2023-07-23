9a7a8eef14fc3b5a1618419866e61702be82ea1b
SenadTopcic <61664503+SenadTopcic@users.noreply.github.com>
2023-07-23 14:27:30 +0100

    Delete part2/courseinfo directory

--- part2/courseinfo/src/components/Header.js
+++
@@ -1,8 +0,0 @@
-//display name of subject
-const Header = ({ course }) => {
-    return (
-      <h1>{course.name}</h1>
-    )
-  }
-
-export default Header
--- part2/courseinfo/src/components/Total.js
+++
@@ -1,8 +0,0 @@
-//display total excersises 
-const Total = ({ course }) => {
-  const total = course.parts.reduce((sum,part) =>{
-    return sum + part.exercises;
-  },0 )
-  return <p><strong>Total of {total} exercises</strong></p>;
-}
-export default Total;
